e32f61c2fdf18686b84ac1a1538724cfa71d201d
semanursirin-7 <semanursirin7@gmail.com>
2026-05-30 13:05:51 +0300

    Update main.cpp
    XOR demo ve entegrasyon main.cpp'ye eklendi

--- main.cpp
+++ main.cpp
@@ -1,7 +1,33 @@
+#include <iostream>
+#include "NeuralNetwork.h"
+#include "DenseLayer.h"
+#include "Activation.h"
 #include "Tests.h"
+using namespace std;
 
-int main()
-{
+int main() {
+    // Testleri çalıştır
     runAllTests();
+    
+    cout << "\n--- XOR Demo ---" << endl;
+    
+    // Ağı kur
+    Sigmoid sigmoid;
+    NeuralNetwork nn;
+    nn.addLayer(new DenseLayer(2, 3, &sigmoid));
+    nn.addLayer(new DenseLayer(3, 1, &sigmoid));
+    
+    // XOR girdileri
+    Matrix input1 = Matrix::FromVector({0.0, 0.0});
+    Matrix input2 = Matrix::FromVector({0.0, 1.0});
+    Matrix input3 = Matrix::FromVector({1.0, 0.0});
+    Matrix input4 = Matrix::FromVector({1.0, 1.0});
+    
+    // Tahminleri ekrana yaz
+    cout << "XOR(0,0) = " << nn.predict(input1)(0,0) << endl;
+    cout << "XOR(0,1) = " << nn.predict(input2)(0,0) << endl;
+    cout << "XOR(1,0) = " << nn.predict(input3)(0,0) << endl;
+    cout << "XOR(1,1) = " << nn.predict(input4)(0,0) << endl;
+    
     return 0;
 }
