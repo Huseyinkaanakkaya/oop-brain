1c998e42e96adb9f74599fff856c5f4a29fff38f
semanursirin-7 <semanursirin7@gmail.com>
2026-05-30 12:45:06 +0300

    Update Tests.cpp
    NeuralNetwork entegrasyon testi eklendi

--- Tests.cpp
+++ Tests.cpp
@@ -1,39 +1,40 @@
 #include <iostream>
 #include "Exceptions.h"
 #include "Tests.h"
-
+#include "NeuralNetwork.h"
+#include "DenseLayer.h"
 using namespace std;
 
-void testDimensionMismatch()
-{
-    try
-    {
+void testDimensionMismatch() {
+    try {
         throw DimensionMismatchException();
-    }
-    catch (const DimensionMismatchException& e)
-    {
+    } catch (const DimensionMismatchException& e) {
         cout << "[TEST BASARILI] " << e.what() << endl;
     }
 }
 
-void testInvalidIndex()
-{
-    try
-    {
+void testInvalidIndex() {
+    try {
         throw InvalidIndexException();
-    }
-    catch (const InvalidIndexException& e)
-    {
+    } catch (const InvalidIndexException& e) {
         cout << "[TEST BASARILI] " << e.what() << endl;
     }
 }
 
-void runAllTests()
-{
-    cout << "Hata yonetimi testleri baslatiliyor..." << endl;
+void testNeuralNetwork() {
+    NeuralNetwork nn;
+    nn.addLayer(new DenseLayer(2, 3));
+    nn.addLayer(new DenseLayer(3, 1));
+    Matrix input = Matrix::FromVector({1.0, 0.5});
+    Matrix output = nn.predict(input);
+    cout << "[TEST BASARILI] NeuralNetwork predict calistiridi, "
+         << "cikti boyutu: " << output.rows() << "x" << output.cols() << endl;
+}
 
+void runAllTests() {
+    cout << "Testler baslatiliyor..." << endl;
     testDimensionMismatch();
     testInvalidIndex();
-
-    cout << "Tum hata testleri tamamlandi." << endl;
+    testNeuralNetwork();
+    cout << "Tum testler tamamlandi." << endl;
 }
